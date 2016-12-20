ac9304f2fa2d82cadde684210491cffacf7a4d8f
Bryan Coburn <greenrhinofl@outlook.com>
2016-12-20 04:42:14 +0000

    Added contact html view

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-  default to: 'info@greenrhinofl.com'
+  default to: 'greenrhinofl@outlook.com'
   
   def contact_email(name, email, body)
     @name = name
